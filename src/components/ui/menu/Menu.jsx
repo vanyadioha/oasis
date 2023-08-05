eaeaa8a2f34db6967783773200d12ccc30cba45f
Victor Anyadioha <victoranyadioha@gmail.com>
2023-08-05 19:20:38 +0100

    Feat/Style: Markup and initial styling for menu bar done

--- src/components/ui/menu/Menu.jsx
+++ src/components/ui/menu/Menu.jsx
@@ -1,7 +1,28 @@
 import { StyledMenu } from './Menu.styled'
+import { navBarLeftButtons, navBarRightButtons } from '@/config/navbarConfig'
+import Link from 'next/link'
 
 export const Menu = () => {
     return (
-        <StyledMenu>Hello</StyledMenu>
+        <StyledMenu>
+            <section className='menu-top'>
+                {navBarLeftButtons.map((i) => {
+                    return (
+                        <Link href={i.route} key={i.label}>
+                            <button type='button'>{i.label}</button>
+                        </Link>
+                    )
+                })}
+            </section>
+            <section className='menu-bottom'>
+                {navBarRightButtons.map((i) => {
+                    return (
+                        <Link href={i.route} key={i.label}>
+                            <button type='button' className={i.route}>{i.label}</button>
+                        </Link>
+                    )
+                })}
+            </section>
+        </StyledMenu>
     )
 }
--- src/components/ui/menu/Menu.styled.js
+++ src/components/ui/menu/Menu.styled.js
@@ -1,4 +1,44 @@
 import { styled } from "styled-components";
 import { motion } from "framer-motion";
 
-export const StyledMenu = styled(motion.div)``;
+export const StyledMenu = styled(motion.div)`
+  position: absolute;
+  height: 100vh;
+  width: 80vw;
+  max-width: 40rem;
+  background-color: moccasin;
+  top: 0;
+  right: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 2rem 1rem;
+  .menu-top,
+  .menu-bottom {
+    display: flex;
+    width: 100%;
+    button {
+      padding: 1.5rem;
+      background-color: inherit;
+      border: none;
+      width: 100%;
+    }
+  }
+  .menu-top {
+    flex-direction: column;
+    button {
+      text-align: left;
+    }
+  }
+  .menu-bottom {
+    a {
+      width: 50%;
+    }
+    .sign-in {
+      border-right: 0.1rem solid grey;
+    }
+    .sign-up {
+      border-left: 0.1rem solid grey;
+    }
+  }
+`;
