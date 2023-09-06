ea5adbd3d742da9ad1a224a831c35574e6712777
Victor Anyadioha <victoranyadioha@gmail.com>
2023-09-06 17:41:26 +0100

    Style: Minor style change to App layout

--- src/components/ui/appLayout/AppLayout.styled.js
+++ src/components/ui/appLayout/AppLayout.styled.js
@@ -4,7 +4,7 @@ export const StyledAppLayout = styled.main`
   width: 100vw;
   display: grid;
   place-items: center;
-  padding: 2rem;
+  padding: 2rem 10rem;
   background-color: var(--bg-primary);
   .inner-layout {
     width: 100%;
