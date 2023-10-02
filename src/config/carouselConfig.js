538d53efa89e20d4ea21d5ed0a439aff2d4c0a85
Victor Anyadioha <victoranyadioha@gmail.com>
2023-10-02 14:21:17 +0100

    Refactor: Change of folder location for assets

--- src/config/carouselConfig.js
+++ src/config/carouselConfig.js
@@ -1,58 +1,57 @@
-import fifalogo from "../assets/carousel assets/logo/fifa.png";
-import gowrlogo from "../assets/carousel assets/logo/gowr.png";
-import gtavlogo from "../assets/carousel assets/logo/gtav.png";
-import hfwlogo from "../assets/carousel assets/logo/hfw.png";
-import smmlogo from "../assets/carousel assets/logo/smm.png";
-import tloulogo from "../assets/carousel assets/logo/tlou.png";
-import fifapix from "../assets/carousel assets/pix/fifa.png";
-import gowrpix from "../assets/carousel assets/pix/gowr.png";
-import gtavpix from "../assets/carousel assets/pix/gtav.png";
-import hfwpix from "../assets/carousel assets/pix/hfw.png";
-import smmpix from "../assets/carousel assets/pix/smm.png";
-import tloupix from "../assets/carousel assets/pix/tlou.png";
-import fifawp from "../assets/carousel assets/wp/fifa.jpg";
-import gowrwp from "../assets/carousel assets/wp/gowr.jpg";
-import gtavwp from "../assets/carousel assets/wp/gtav.jpg";
-import hfwwp from "../assets/carousel assets/wp/hfw.png";
-import smmwp from "../assets/carousel assets/wp/smm.jpg";
-import tlouwp from "../assets/carousel assets/wp/tlou.jpg";
+import fifalogo from "../../public/assets/carousel/logo/fifa.png";
+import gowrlogo from "../../public/assets/carousel/logo/gowr.png";
+import gtavlogo from "../../public/assets/carousel/logo/gtav.png";
+import hfwlogo from "../../public/assets/carousel/logo/hfw.png";
+import smmlogo from "../../public/assets/carousel/logo/smm.png";
+import tloulogo from "../../public/assets/carousel/logo/tlou.png";
+import fifapix from "../../public/assets/carousel/pix/fifa.png";
+import gowrpix from "../../public/assets/carousel/pix/gowr.png";
+import gtavpix from "../../public/assets/carousel/pix/gtav.png";
+import hfwpix from "../../public/assets/carousel/pix/hfw.png";
+import smmpix from "../../public/assets/carousel/pix/smm.png";
+import tloupix from "../../public/assets/carousel/pix/tlou.png";
 
 export const carouselConfig = [
   {
     name: "FIFA 23",
+    bgTag: "fifa.jpg",
     logo: fifalogo,
     pix: fifapix,
-    bg: fifawp,
+    objectFit: "contain",
   },
   {
     name: "God Of War: Ragnarok",
+    bgTag: "gowr.jpg",
     logo: gowrlogo,
     pix: gowrpix,
-    bg: gowrwp,
+    objectFit: "contain",
   },
   {
     name: "Grand Theft Auto V",
+    bgTag: "gtav.jpg",
     logo: gtavlogo,
     pix: gtavpix,
-    bg: gtavwp,
+    objectFit: "contain",
   },
   {
     name: "Horizon: Forbidden West",
+    bgTag: "hfw.png",
     logo: hfwlogo,
     pix: hfwpix,
-    bg: hfwwp,
+    objectFit: "contain",
   },
   {
     name: "Spider-Man: Miles Morales",
+    bgTag: "smm.jpg",
     logo: smmlogo,
     pix: smmpix,
-    bg: smmwp,
+    objectFit: "cover",
   },
   {
     name: "The Last of Us Part II",
+    bgTag: "tlou.jpg",
     logo: tloulogo,
     pix: tloupix,
-    // bg: tlouwp,
-    bg: "../src/assets/carousel assets/wp/tlou.jpg",
+    objectFit: "contain",
   },
 ];
