3f0e3ba14b1138e9e3f6690979dbe34f65a8a998
Victor Anyadioha <victoranyadioha@gmail.com>
2023-11-01 11:03:49 +0100

    Style: Responsive CSS for PopularGames.jsx

--- src/components/pages/home/popular-games/PopularGames.jsx
+++ src/components/pages/home/popular-games/PopularGames.jsx
@@ -1,31 +1,34 @@
+import { Skeleton } from '@/components/ui'
 import { StyledPopularGames } from './PopularGames.styled'
 import { useAxiosGet } from '@/hooks'
 import Image from 'next/image'
+import { Suspense } from 'react'
 
 export const PopularGames = ({ apiKey, baseUrl }) => {
     const { data, loading, error } = useAxiosGet(`${baseUrl}/games?key=${apiKey}&page=1&page_size=10`)
     return (
         <StyledPopularGames>
             <h1>Games that you love <span>{'>'}</span></h1>
-            <ul className='popular-games'>
-                {loading && 'Loading...'}
-                {data && data.results.map((i) => {
-                    return (
-                        <li key={i.slug} className='popular-game'>
-                            <div className="game-img-container">
-                                {i.background_image && <Image
-                                    src={i.background_image}
-                                    alt={i.slug}
-                                    width={1280}
-                                    height={720}
-                                />}
-                            </div>
-                            <h3>{i.name}</h3>
-                        </li>
-                    )
-                })}
-                {error && 'Something is wrong'}
-            </ul>
+            {loading && <Skeleton />}
+            {data &&
+                <ul className='popular-games'>
+                    {data.results.map((i) => {
+                        return (
+                            <li className='popular-game' key={i.slug}>
+                                <div className="game-img-container">
+                                    {i.background_image && <Image
+                                        src={i.background_image}
+                                        alt={i.slug}
+                                        width={1280}
+                                        height={720}
+                                    />}
+                                </div>
+                                <h3>{i.name}</h3>
+                            </li>
+                        )
+                    })}
+                </ul>}
+            {error && 'Something is wrong'}
         </StyledPopularGames>
     )
 }
--- src/components/pages/home/popular-games/PopularGames.styled.js
+++ src/components/pages/home/popular-games/PopularGames.styled.js
@@ -3,25 +3,34 @@ import styled from "styled-components";
 
 export const StyledPopularGames = styled(motion.section)`
   margin-top: 2rem;
+  width: 100%;
+
   h1 {
     margin-bottom: 2rem;
   }
   .popular-games {
+    width: 100%;
     display: grid;
     gap: 2rem;
     grid-template-columns: repeat(auto-fill, minmax(24rem, 1fr));
     grid-auto-rows: auto;
   }
   .popular-game {
-    background-color: gray;
     width: 100%;
   }
   .game-img-container {
     width: 100%;
     height: 13.5rem;
     /* height: calc((9 * width) / 16); */
-    img {
-      object-position: top;
+  }
+  @media (max-width: 780px) {
+    .popular-games {
+      display: flex;
+      overflow: scroll;
+      padding: 1rem 0;
+    }
+    .popular-game {
+      min-width: 16rem;
     }
   }
 `;
