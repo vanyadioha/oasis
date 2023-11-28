a16f50ac75cf8d08567c077b0a689bae3e95fde2
Victor Anyadioha <victoranyadioha@gmail.com>
2023-11-28 01:02:42 +0100

    FEAT: Implemented a scroll to top button

--- src/components/ui/appLayout/AppLayout.jsx
+++ src/components/ui/appLayout/AppLayout.jsx
@@ -1,10 +1,20 @@
 import { StyledAppLayout } from './AppLayout.styled'
+import { AiOutlineArrowUp as AOAU } from 'react-icons/ai'
+import { useMediaQuery } from '@/hooks'
 
 export const AppLayout = ({ children }) => {
+    const isSmallDevice = useMediaQuery('only screen and (max-width: 780px)')
+    const handleScrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        });
+    };
     return (
         <StyledAppLayout>
             <div className='inner-layout'>
                 {children}
+                <button type='button' id='to-top' onClick={handleScrollToTop}><AOAU size={isSmallDevice ? 25 : 35} /></button>
             </div>
         </StyledAppLayout>
     )
--- src/components/ui/appLayout/AppLayout.styled.js
+++ src/components/ui/appLayout/AppLayout.styled.js
@@ -10,16 +10,36 @@ export const StyledAppLayout = styled.main`
     width: calc(100vw - 20rem);
     max-width: 144rem;
   }
+  #to-top {
+    position: fixed;
+    bottom: 5rem;
+    right: 10rem;
+    z-index: 2;
+    height: 6rem;
+    width: 6rem;
+    border-radius: 50%;
+    background-color: var(--text-secondary);
+    border: none;
+  }
   @media only screen and (max-width: 1320px) {
     padding: 2rem 5rem;
     .inner-layout {
       width: calc(100vw - 10rem);
+    }
+    #to-top {
+      right: 5rem;
     }
   }
   @media only screen and (max-width: 780px) {
     padding: 2rem;
     .inner-layout {
       width: calc(100vw - 4rem);
+    }
+    #to-top {
+      right: 3rem;
+      bottom: 3rem;
+      height: 5rem;
+      width: 5rem;
     }
   }
 `;
