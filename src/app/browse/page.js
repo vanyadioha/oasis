62aa495f5f44542a7c50ec753d4560bf9653f404
Victor Anyadioha <victoranyadioha@gmail.com>
2023-11-29 15:03:31 +0100

    FEAT: Created a route for the browse page

--- src/app/browse/page.js
+++ src/app/browse/page.js
@@ -1,3 +1,7 @@
+import { Browse as BrowseGames } from "@/components/pages/browse";
+
 export default function Browse() {
-  return <>Browse games hiaaaaa!</>;
+  const baseUrl = process.env.BASE_URL;
+  const apiKey = process.env.API_KEY;
+  return <BrowseGames baseUrl={baseUrl} apiKey={apiKey} />;
 }
---
+++ src/components/pages/browse/browse-games/Browse.jsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { StyledBrowse } from "./Browse.styled";
+
+export const Browse = ({ baseUrl, apiKey }) => {
+    return (
+        <StyledBrowse>
+            <h1>BROWSE</h1>
+        </StyledBrowse>
+    )
+}
